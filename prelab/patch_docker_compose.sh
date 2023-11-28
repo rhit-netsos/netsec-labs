4f74416cdd522b74c854ae01729ace95688bc726
Mohammad Noureddine <nouredd2@users.noreply.github.com>
2023-11-28 13:45:21 -0500

    prelab 1 ready for students

---
+++ prelab/patch_docker_compose.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Patch the docker-compose file with each username, otherwise networks will not
+# be created.
+
+STUDENT=$1
+SUBNET=$2
+
+if [ -z $STUDENT ]; then
+  echo "Please enter you student id"
+  exit 99
+fi
+
+if [ -z $SUBNET ]; then
+  echo "Please enter your subnet as the second argument."
+  exit 99
+fi
+
+# patch hostA
+PATTERN="s/hostA/${STUDENT}-hostA/g"
+sed -i $PATTERN docker-compose.yml
+
+# patch hostB
+PATTERN="s/hostB/${STUDENT}-hostB/g"
+sed -i $PATTERN docker-compose.yml
+
+# patch network
+PATTERN="s/local-net/${STUDENT}-local-net/g"
+sed -i $PATTERN docker-compose.yml
+
+# patch network subnet
+PATTERN="s/10.10.0/${SUBNET}/g"
+sed -i $PATTERN docker-compose.yml
+
+echo -e "Done...."
+
