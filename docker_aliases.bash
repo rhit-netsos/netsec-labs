34a24c41e1cc502c91af1c7cbe3e795c9a6df844
mnoureddine <nouredd2@users.noreply.github.com>
2023-11-30 12:37:57 -0500

    initial aliases file to help out with docker stuff

---
+++ docker_aliases.bash
@@ -0,0 +1,30 @@
+# a bunch of aliases to make life easier in the class
+
+# vim
+alias v='vim'
+
+# tmux stuff
+alias tl='tmux ls'
+alias ta='tmux at -t'
+alias ts='tmux new'
+
+# docker compose stuff
+alias dco='docker compose'
+alias dcps='docker compose ps'
+alias dcrestart='docker compose restart'
+alias dcupd='docker compse up -d'
+alias dcdn='docker compose down'
+alias dcstop='docker compose stop'
+
+# docker stuff
+alias dxcit='docker exec -it'
+alias dst='docker container start'
+alias drs='docker container restart'
+alias dstp='docker container stop'
+alias dcls='docker container ls'
+alias dcin='docker container inspect'
+
+# network stuff
+alias dnls='docker network ls'
+
+
