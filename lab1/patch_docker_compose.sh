babd39ab0450892fc0f7c4bcf1413e3c1a66c8ea
Mohammad Noureddine <noureddi@rose-hulman.edu>
2023-11-30 12:25:02 -0500

    forgot to push lab1

---
+++ lab1/patch_docker_compose.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Patch the docker-compose file with each username, otherwise networks will not
+# be created.
+
+STUDENT=$1
+SUBNET=$2
+
+if [ -z $STUDENT ]; then
+  echo "Please enter you student id"
+  exit 99
+fi
+
+if [ -z $SUBNET ]; then
+  echo "Please enter your subnet as the second argument."
+  exit 99
+fi
+
+# patch hosts
+PATTERN="s/\(host[A-Z]\)/${STUDENT}-\1/g"
+sed -i $PATTERN docker-compose.yml
+
+# patch attacker
+PATTERN="s/attacker/${STUDENT}-attacker/g"
+sed -i $PATTERN docker-compose.yml
+
+# patch hostA
+# PATTERN="s/hostA/${STUDENT}-hostA/g"
+# sed -i $PATTERN docker-compose.yml
+
+# patch hostB
+# PATTERN="s/hostB/${STUDENT}-hostB/g"
+# sed -i $PATTERN docker-compose.yml
+
+# patch network
+PATTERN="s/local-net/${STUDENT}-local-net/g"
+sed -i $PATTERN docker-compose.yml
+
+# patch network subnet
+PATTERN="s/10.10.0/${SUBNET}/g"
+sed -i $PATTERN docker-compose.yml
+
+echo -e "Done...."
+
